4c21668c442cbb1b544368bfced5e1a1e6b38764
Haïder Alshamma <haideralsh@gmail.com>
2020-12-03 19:23:57 -0500

    Support a wildcard pattern
    The wildcard pattern allows a match for any case.
    It is the equivalent of default in a switch-case.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const WILDCARD_VALUE =
+  "__matchmaker__wildcard__caf2b6d7-0f49-4575-bccb-bc8ccca57ae8";
+
 const match = (...args) => {
   return (...whens) => {
     const res = whens
@@ -12,19 +15,24 @@ const when = (pattern, result) => (args) => {
   if (pattern.length != args.length) return { matches: false };
 
   for (let i = 0; i < pattern.length; ++i) {
+    if (pattern[i] === WILDCARD_VALUE) continue;
     if (pattern[i] !== args[i]) return { matches: false };
   }
 
   return { matches: true, result };
 };
 
-let a = "a",
-  b = "b",
-  c = "c";
+const _ = WILDCARD_VALUE;
 
-const positive = match(a, b, c)(when(["a", "b", "c"], "something"));
-const negative = match(a, b, c)(when(["d", "e", "f"], "something"));
-const lengthMismatch = match(a, b, c)(when(["a", "b"], "something"));
-console.log("Positive: ", positive);
-console.log("Negative: ", negative);
-console.log("Length Mismatch: ", lengthMismatch);
+const color = (r, g, b, a) =>
+  match(r, g, b, a)(
+    when([_, _, _, 0], "Transparent"),
+    when([255, 13, 255, _], "Magenta"),
+    when([255, 83, 13, _], "Orange"),
+    when([49, 193, 175, _], "Turquoise")
+  );
+
+console.log("Transparent:", color(255, 13, 255, 0));
+console.log("Magenta:", color(255, 13, 255, 100));
+console.log("Orange:", color(255, 83, 13, 100));
+console.log("Turquoise:", color(49, 193, 175, 100));
