08118e82fdadeb30f4c126b289127365720d9a35
Haïder Alshamma <haideralsh@gmail.com>
2020-12-04 20:25:42 -0500

    Move debugging code to `/test.ts`

--- index.ts
+++ index.ts
@@ -30,16 +30,3 @@ export const when = (pattern: any[], result: any): Function => (
 }
 
 export const _ = WILDCARD_VALUE
-
-const color = (r: number, g: number, b: number, a: number) =>
-    match(r, g, b, a)(
-        when([_, _, _, 0], 'Transparent'),
-        when([255, 13, 255, _], 'Magenta'),
-        when([255, 83, 13, _], 'Orange'),
-        when([49, 193, 175, _], 'Turquoise')
-    )
-
-console.log('Transparent:', color(255, 13, 255, 0))
-console.log('Magenta:', color(255, 13, 255, 100))
-console.log('Orange:', color(255, 83, 13, 100))
-console.log('Turquoise:', color(49, 193, 175, 100))
