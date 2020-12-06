938284efe68533905e2f06f9f66352ddbe5b32fa
Haïder Alshamma <haideralsh@gmail.com>
2020-12-06 14:17:27 -0500

    Add unit tests for `when` and `_`

--- index.test.ts
+++ index.test.ts
@@ -1,18 +1,92 @@
 import { match, when, _ } from '.'
 
-const color = (r: number, g: number, b: number, a: number) =>
-    match(r, g, b, a)(
-        when([_, _, _, 0], 'Transparent'),
-        when([255, 13, 255, _], 'Magenta'),
-        when([255, 83, 13, _], 'Orange'),
-        when([49, 193, 175, _], 'Turquoise')
-    )
+describe('when function', () => {
+    // Behaviour
+    test("returns a { match: false } when the pattern array's length is not the same as the values array's length", () => {
+        expect(when([1, 2, 3, 4], 'result')([])).toEqual({ matches: false })
+    })
 
-describe('color function', () => {
-    test('Returns the right color when given the right numbers', () => {
-        expect(color(255, 13, 255, 0)).toBe('Transparent')
-        expect(color(255, 13, 255, 100)).toBe('Magenta')
-        expect(color(255, 83, 13, 100)).toBe('Orange')
-        expect(color(49, 193, 175, 100)).toBe('Turquoise')
+    test('returns a { match: false } when any value is does not match', () => {
+        expect(when([1, 2, 3, 4], 'result')([1, 2, 4, 3])).toEqual({
+            matches: false,
+        })
+    })
+
+    test('returns a { match: true } with the result if all values match', () => {
+        expect(when([1, 2, 3, 4], 'result')([1, 2, 3, 4])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('does not consider wildcard matcher', () => {
+        expect(when([_], 'result')(['a'])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    // Support for primitives
+    test('supports numbers matching', () => {
+        expect(when([1, 2, 3], 'result')([1, 2, 3])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('supports strings matching', () => {
+        expect(
+            when(['foo', 'bar', 'baz'], 'result')(['foo', 'bar', 'baz'])
+        ).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('supports BigInt matching', () => {
+        expect(
+            when(
+                [BigInt(9007199254740991)],
+                'result'
+            )([BigInt(9007199254740991)])
+        ).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    // Untruthy values
+    test('supports null matching', () => {
+        expect(when([null], 'result')([null])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('supports undefined matching', () => {
+        expect(when([undefined], 'result')([undefined])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('supports empty string matching', () => {
+        expect(when([''], 'result')([''])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+
+    test('supports number 0 matching', () => {
+        expect(when([0], 'result')([0])).toEqual({
+            matches: true,
+            result: 'result',
+        })
+    })
+})
+
+describe('wildcard matcher `_`', () => {
+    test('is a wildcard matcher', () => {
+        expect(match('result')(when([_], 'result'))).toBe('result')
     })
 })
