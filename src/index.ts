1b227d1438fbbc3cf14166bece8bfbe51255e1a4
Haïder Alshamma <haideralsh@gmail.com>
2020-12-09 20:08:30 -0500

    Correct comments

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,13 @@ type NegativeMatch = {
 }
 
 /**
- * MatchResult is an Algebric Data Type that defines the return type for the
+ * `MatchResult` is an Algebric Data Type that defines the return type for the
  * {@link when} function.
  */
 type MatchResult = PositiveMatch | NegativeMatch
 
 /**
- * match is a higher order function that takes any number of values of any type
+ * `match` is a higher order function that takes any number of values of any type
  * and returns a function that accepts any number of functions that have a
  * return type of {@link MatchResult}
  */
@@ -31,12 +31,12 @@ const match = (...values: any[]) => (...whens: Function[]) =>
         .find(({ matches, result }) => matches && result)?.result
 
 /**
- * when is a higher order function that receives an array of the same length as
+ * `when` is a higher order function that receives an array of the same length as
  * the values provided to the parent {@link match} function, and a result which
  * will be returned as the `result` in {@link PositiveMatch} in case of a
  * positive match.
  *
- * It performs the pattern matching using reference type and value equality. It
+ * `when` performs the pattern matching using reference type and value equality. It
  * returns a negative match if the number of values given to its parent
  * {@link match} function is not the same as the number of patterns it is given,
  * or if any of the values do not match their corresponding pattern value.
