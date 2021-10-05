8d082ce30da45ce796b682750fd72aa725f0b6a8
haideralsh <haideralsh@users.noreply.github.com>
2021-10-05 16:06:44 +0000

    Format code

--- src/index.ts
+++ src/index.ts
@@ -45,10 +45,12 @@ type MatchResult = PositiveMatch | NegativeMatch
  * and returns a function that accepts any number of functions that have a
  * return type of {@link MatchResult}
  */
-const match = (...values: ReadonlyArray<any>) => (...whens: Function[]) =>
-    whens
-        .map((when) => when(values))
-        .find(({ matches, result }) => matches && result)?.result
+const match =
+    (...values: ReadonlyArray<any>) =>
+    (...whens: Function[]) =>
+        whens
+            .map((when) => when(values))
+            .find(({ matches, result }) => matches && result)?.result
 
 /**
  * `when` is a higher order function that receives an array of the same length as
@@ -61,31 +63,33 @@ const match = (...values: ReadonlyArray<any>) => (...whens: Function[]) =>
  * {@link match} function is not the same as the number of patterns it is given,
  * or if any of the values do not match their corresponding pattern value.
  */
-const when = (pattern: ReadonlyArray<any>, result: any): Function => (
-    values: ReadonlyArray<any>
-): MatchResult => {
-    if (pattern.length != values.length) return { matches: false }
+const when =
+    (pattern: ReadonlyArray<any>, result: any): Function =>
+    (values: ReadonlyArray<any>): MatchResult => {
+        if (pattern.length != values.length) return { matches: false }
 
-    for (let i = 0; i < pattern.length; ++i) {
-        if (pattern[i] === WILDCARD_VALUE) continue
+        for (let i = 0; i < pattern.length; ++i) {
+            if (pattern[i] === WILDCARD_VALUE) continue
 
-        if (pattern[i]?.operator) {
-            const { operator, elements }: Operation = pattern[i]
-            switch (operator) {
-                case 'either':
-                    if (elements.some((elem) => elem === values[i])) continue
-                    else return { matches: false }
+            if (pattern[i]?.operator) {
+                const { operator, elements }: Operation = pattern[i]
+                switch (operator) {
+                    case 'either':
+                        if (elements.some((elem) => elem === values[i]))
+                            continue
+                        else return { matches: false }
 
-                case 'neither':
-                    if (elements.every((elem) => elem !== values[i])) continue
-                    else return { matches: false }
+                    case 'neither':
+                        if (elements.every((elem) => elem !== values[i]))
+                            continue
+                        else return { matches: false }
+                }
             }
+
+            if (pattern[i] !== values[i]) return { matches: false }
         }
 
-        if (pattern[i] !== values[i]) return { matches: false }
+        return { matches: true, result }
     }
-
-    return { matches: true, result }
-}
 
 export { match, when, neither, either, WILDCARD_VALUE as _ }
